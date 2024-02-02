60340fc6c4f1238f0a4560e5239b77bdee211c78
roentm <bloodythewilliam@gmail.com>
2024-02-02 21:20:19 +0200

    scheme

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,14 +1,33 @@
 import mongoose from "mongoose";
-// Amitoz 27/01/24
-// Wating to Cahnges....
+// eliran 30/01/24
 const ingredientSchema = new mongoose.Schema({
-    name: String,
-    category: {
-        type:String,
-        required:true,
-        match:["Flours","Grains","Nuts","Fruits&Vegtables","Herbs&Spices","Fats","Meats","Dairy","Vegan"]
-    },
-   
+  name: String,
+  category: {
+    type: String,
+    required: true,
+    match: [
+      "Flours",
+      "Grains",
+      "Nuts",
+      "Fruits&Vegtables",
+      "Herbs&Spices",
+      "Fats",
+      "Meats",
+      "Dairy",
+      "Vegan",
+      "Preserves",
+      "Other",
+    ],
+  },
+  info: [String],
+  ingredMeasure: [String],
+  ingredScaleToGr: [Number],
+  allergens: [String],
+  indgredCarbs: [Number],
+  indgredProt: [Number],
+  indgredSugar: [Number],
+  ingredFat: [Number],
+  indgredSodium: [Number],
 });
 
 export const Ingredient = mongoose.model("ingredients", ingredientSchema);
---
+++ models/recipe.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+// eliran 30/01/24
+const recipeSchema = new mongoose.Schema({
+  name: String,
+  //כאן כנראה שנחליף בטבלת יצירה כללית לשרת
+  creation: {
+    cDate: {
+      type: Date,
+      default: Date.now,
+    },
+    creatorID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  //כאן כנראה שנחליף בטבלת עדכונים כללית לשרת
+  updateLog: [
+    {
+      uDate: Date,
+      uID: mongoose.Schema.Types.ObjectId,
+      content: [String],
+    },
+  ],
+  themeIMG: String,
+  coverIMG: String,
+  recipeIMG: [String],
+  rImgCap: [String],
+  tags: [String],
+  prepTime: Number,
+  cookTime: Number,
+  difficulty: {
+    type: Number,
+    min: 0,
+    max: 3,
+  },
+  adjustable: Boolean,
+  intro: [String],
+  body: [
+    //אובייקט של שלב במתכון
+    {
+      sName: String,
+      sIngred: [mongoose.Schema.Types.ObjectId],
+      sIngredQt: [Number],
+      sIngredMsr: [String],
+      sAltIngred: [mongoose.Schema.Types.ObjectId],
+      sIngredIndex: [Number],
+      sAltQt: [Number],
+      sAltMsr: [String],
+      sIMG: [String],
+      sImgCap: [String],
+      sInstruct: [String],
+      sTip: [String],
+    },
+  ],
+  tips: [String],
+  closing: [String],
+});
+
+export const recipe = mongoose.model("recipes", recipeSchema);
