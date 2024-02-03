e59143bfd35065aaee058ed8a09a538f30300ec5
roentm <bloodythewilliam@gmail.com>
2024-02-03 02:48:48 +0200

    Update services.js

--- services.js
+++ services.js
@@ -1,24 +1,31 @@
-import { authMiddleware } from "./middleware/authMiddleware.js"
-import { ForgotPassword } from "./services/users/ForgotPassword.js"
-import { Login } from "./services/users/Login.js"
-import { Register } from "./services/users/Register.js"
-import { activeUser } from "./services/users/activeUser.js"
-import { updateUser } from "./services/users/updateUser.js"
+import { authMiddleware } from './middleware/authMiddleware.js';
+import {
+  Register,
+  activeUser,
+  Login,
+  ForgotPassword,
+  updateUser,
+} from './services';
 
+export const services = (app) => {
+  // Amitoz 27/01/24
+  app.post('/user/register', Register);
+  app.post('/user/login', Login);
+  app.get(
+    '/user/check',
+    authMiddleware,
+    activeUser
+  );
+  app.get('/user/activeUser', activeUser);
+  app.post(
+    '/user/ForgotPassword',
+    ForgotPassword
+  ); // 30/01/24
+  app.post(
+    '/user/updateUser',
+    authMiddleware,
+    updateUser
+  );
 
-
-export const services= (app)=>{
-    // Amitoz 27/01/24    
-    app.post('/user/register',Register)
-    app.post('/user/login',Login)
-    app.get('/user/check',authMiddleware,activeUser)
-    app.get('/user/activeUser',activeUser)
-    app.post('/user/ForgotPassword',ForgotPassword) // 30/01/24
-    app.post('/user/updateUser',authMiddleware,updateUser)
-    
-    // END
-
-    
-    
-    
-}
+  // END
+};
