71d213aedc09755c4d2ad71ce355398d1b6f9922
ozamitoz1 <117310878+ozamitoz1@users.noreply.github.com>
2024-02-04 19:12:34 +0200

    ok

--- models/docCreation.js
+++ models/docCreation.js
@@ -1,15 +1,16 @@
-// import mongoose from "mongoose";
-// // eliran 02/02/24
-// const docCreationSchema = new mongoose.Schema({
-//   docType: {
-//     type: String,
-//     match: ["user", "recipe", "ingrediant"],
-//   },
-//   creationDate: Date,
-//   //זיהוי המשתמש היוצר
-//   userID: mongoose.Schema.Types.ObjectId,
-//   //זיהוי הרשומה שנוצרה
-//   docID: mongoose.Schema.Types.ObjectId,
-// });
+import mongoose from "mongoose";
 
-// export const DocCreation = mongoose.model("docupdates", docCreationSchema);
+// eliran 02/02/24
+const docCreationSchema = new mongoose.Schema({
+  docType: {
+    type: String,
+    match: ["user", "recipe", "ingrediant"],
+  },
+  creationDate: Date,
+  //זיהוי המשתמש היוצר
+  userID: mongoose.Schema.Types.ObjectId,
+  //זיהוי הרשומה שנוצרה
+  docID: mongoose.Schema.Types.ObjectId,
+});
+
+export const DocCreation = mongoose.model("docupdates", docCreationSchema);
