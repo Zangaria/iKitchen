4ebcbb8e1699edccefd2d4f0972c964ae29dc83d
roentm <bloodythewilliam@gmail.com>
2024-02-04 19:18:46 +0200

    docupdates

--- models/docUpdate.js
+++ models/docUpdate.js
@@ -1,16 +1,16 @@
-// import mongoose from "mongoose";
-// // eliran 02/02/24
-// const docUpdateSchema = new mongoose.Schema({
-//   docType: {
-//     type: String,
-//     match: ["user", "recipe", "ingrediant"],
-//   },
-//   uDate: Date,
-//   //זיהוי המשתמש היוצר
-//   userID: mongoose.Schema.Types.ObjectId,
-//   //זיהוי הרשומה שנוצרה
-//   docID: mongoose.Schema.Types.ObjectId,
-//   editlog: [String],
-// });
+import mongoose from "mongoose";
+// eliran 02/02/24
+const docUpdateSchema = new mongoose.Schema({
+  docType: {
+    type: String,
+    match: ["user", "recipe", "ingrediant"],
+  },
+  uDate: Date,
+  //זיהוי המשתמש היוצר
+  userID: mongoose.Schema.Types.ObjectId,
+  //זיהוי הרשומה שנוצרה
+  docID: mongoose.Schema.Types.ObjectId,
+  editlog: [String],
+});
 
-// export const DocUpdate = mongoose.model("docupdates", docUpdateSchema);
+export const DocUpdate = mongoose.model("docupdates", docUpdateSchema);
--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-//import { DocCreation } from "./docCreation.js";
+import { DocCreation } from "./docCreation.js";
 import { Ingredient } from "./ingredient.js";
 import { recipe } from "./recipe.js";
-//import { DocUpdate } from "./docUpdate.js";
+import { DocUpdate } from "./docUpdate.js";
 import { User } from "./User.js";
 
-export { Ingredient, recipe, User };
+export { Ingredient, recipe, User, DocUpdate, DocCreation };
