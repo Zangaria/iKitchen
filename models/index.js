b997f3f80a774819eaf281faccd52f2b1db6947c
ozamitoz1 <117310878+ozamitoz1@users.noreply.github.com>
2024-02-04 19:28:59 +0200

    OK

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-import { DocCreation } from "./docCreation.js";
+import { User } from "./User.js";
+import { DocUpdate } from "./docUpdate.js";
 import { Ingredient } from "./ingredient.js";
 import { recipe } from "./recipe.js";
-import { DocUpdate } from "./docUpdate.js";
-import { User } from "./User.js";
 
-export { Ingredient, recipe, User, DocUpdate, DocCreation };
+export { Ingredient, recipe, User, DocUpdate };
+
