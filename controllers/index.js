5c53632493bf24399ee89640b31624a6896e1736
roentm <bloodythewilliam@gmail.com>
2024-02-04 17:40:42 +0200

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,11 @@
-import { activeUserById } from './users/activeUserByID.js';
-import { addUser } from './users/addUser.js';
-import { forgotPass } from './users/forgotPass.js';
-import { loginUser } from './users/login.js';
-import { updateUserById } from './users/updateUserById.js';
+import { activeUserById } from "./users/activeUserByID.js";
+import { addUser } from "./users/addUser.js";
+import { forgotPass } from "./users/forgotPass.js";
+import { loginUser } from "./users/login.js";
+import { updateUserById } from "./users/updateUserById.js";
+import { addIngrediant } from "./ingredients/addIngredient.js";
+import { addRecipe } from "./recipes/addRecipe.js";
 
-export {
-  activeUserById,
-  addUser,
-  forgotPass,
-  loginUser,
-  updateUserById,
-};
+export { activeUserById, addUser, forgotPass, loginUser, updateUserById };
+export { addIngrediant };
+export { addRecipe };
