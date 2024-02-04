8797084d5e9cceca289b517491601ee559e2b963
roentm <bloodythewilliam@gmail.com>
2024-02-04 19:34:27 +0200

    uniting creation and update

--- models/docUpdate.js
+++ models/docUpdate.js
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
 // eliran 02/02/24
 const docUpdateSchema = new mongoose.Schema({
+  docRef: {
+    type: String,
+    match: ["creation", "update"],
+    require: true,
+  },
   docType: {
     type: String,
-    match: ["user", "recipe", "ingrediant"],
+    match: ["user", "recipe", "ingredient"],
+    require: true,
   },
   uDate: Date,
-  //זיהוי המשתמש היוצר
+  //זיהוי המשתמש המעדכן
   userID: mongoose.Schema.Types.ObjectId,
-  //זיהוי הרשומה שנוצרה
+  //זיהוי הרשומה שעודכנה
   docID: mongoose.Schema.Types.ObjectId,
   editlog: [String],
+  editNote: String,
 });
 
 export const DocUpdate = mongoose.model("docupdates", docUpdateSchema);
