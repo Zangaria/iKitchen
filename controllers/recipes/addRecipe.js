a999a4d93439d2549055920b2cfb8e2b6800e7c4
roentm <bloodythewilliam@gmail.com>
2024-02-04 19:01:58 +0200

    Update addRecipe.js

--- controllers/recipes/addRecipe.js
+++ controllers/recipes/addRecipe.js
@@ -7,21 +7,8 @@ export const addRecipe = async (data) => {
   if (
     !data?.name ||
     !data?.themeIMG ||
-    !data?.cookTime ||
-    !data?.difficulty ||
-    !data?.adjustable ||
-    !data?.themeIMG ||
     data.tags.length() == 0 ||
-    data.body.length() == 0 ||
-    data.body.sIngred.length() == 0 ||
-    data.body.sIngredQt.length() != data.body.sIngred.length() ||
-    data.body.sIngredMsr.length() != data.body.sIngred.length() ||
-    data.body.sIngredIndex.length() != data.body.sAlt.length() ||
-    data.body.sAltQt.length() != data.body.sAlt.length() ||
-    data.body.sAltMsr.length() != data.body.sAlt.length() ||
-    data.body.sImgCap.length() != data.body.sIMG.length() ||
-    data.body.sInstruct.length() == 0 ||
-    data.closing.length() == 0
+    data.body.length() == 0
   ) {
     return {
       code: 106,
