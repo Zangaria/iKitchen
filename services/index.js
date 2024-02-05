94d98675fafde71fb298ba4903f67ea2c7ddb219
roentm <bloodythewilliam@gmail.com>
2024-02-06 01:39:49 +0200

    Update index.js

--- services/index.js
+++ services/index.js
@@ -4,15 +4,10 @@ import { ForgotPassword } from './users/ForgotPassword.js';
 import { Login } from './users/Login.js';
 import { Register } from './users/Register.js';
 import { updateUser } from './users/updateUser.js';
-import { addToWeb, getDataWeb } from './web/web.js';
-
-export {
-  activeUser,
-  ForgotPassword,
-  Login,
-  Register,
-  updateUser,
-  getDataWeb,
+import {
   addToWeb,
-  addIngredient
-};
+  getDataWeb,
+} from './web/web.js';
+
+export {activeUser,ForgotPassword,Login,Register,updateUser,addToWeb,getDataWeb};
+export{addIngredient};
