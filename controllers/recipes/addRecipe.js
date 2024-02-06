6e1df46ed2cb9832dd3b6887e8c64667c34878ea
roentm <bloodythewilliam@gmail.com>
2024-02-06 18:23:37 +0200

    Update addRecipe.js

--- controllers/recipes/addRecipe.js
+++ controllers/recipes/addRecipe.js
@@ -1,4 +1,8 @@
 import { recipe } from "../../models/index.js";
+import { createDoc } from "../docUpdates/createDoc.js";
+
+// Eliran 06/02/24
+// adding the create record functionality
 
 // Eliran 04/02/24
 // data = {token:'toekn', and paramaeter you want update the params need be like the schema!!!!!}
@@ -20,6 +24,16 @@ export const addRecipe = async (data) => {
     //create a new recipe
     const newRecipe = new recipe(data);
     const res = await newRecipe.save();
+
+    const docRef = "creation";
+    const docType = "recipe";
+    const userID = data.cUser;
+    const docID = res?._id;
+    const recordCreation = { docRef, docType, uDate, userID, docID };
+    const recordRes = await createDoc(recordCreation);
+    if (recordRes?.err) {
+      console.log(recordRes.msg);
+    }
     return res?._id.toString();
   } catch (error) {
     return { err: true, msg: error.message };
