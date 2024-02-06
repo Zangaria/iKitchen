c2c77966eacaf259a0196bf3e160e161a9417b3b
roentm <bloodythewilliam@gmail.com>
2024-02-06 17:08:17 +0200

    finalizing the add ingredient service

--- controllers/ingredients/addIngredient.js
+++ controllers/ingredients/addIngredient.js
@@ -1,14 +1,21 @@
+import { getIsraelDateTime } from "../../helpers/getdate.js";
 import { Ingredient } from "../../models/index.js";
+import { createDoc } from "../docUpdates/createDoc.js";
 
 // Eliran 02/02/24
 // data = {token:'toekn', and paramaeter you want update the params need be like the schema!!!!!}
 // use this func to update user , user password or... username......
 export const addIngrediant = async (data) => {
-  if (!data?.name || !data?.category || data.info.length() == 0) {
+  if (
+    !data?.name ||
+    !data?.category ||
+    !data?.cUser ||
+    data.info.length() == 0
+  ) {
     return {
       code: 106,
       err: true,
-      msg: "the parameters name & category & info are required.",
+      msg: "the parameters name & category & cUser & info are required.",
     };
   }
   try {
@@ -26,7 +33,18 @@ export const addIngrediant = async (data) => {
     else {
       //create a new ingrediant
       const newIngredient = new Ingredient(data);
+      newIngredient.cDate = getIsraelDateTime();
       const res = await newIngredient.save();
+      //record the creation on the updates table
+      const docRef = "creation";
+      const docType = "ingredient";
+      const userID = data.cUser;
+      const docID = response;
+      const recordCreation = { docRef, docType, uDate, userID, docID };
+      const recordRes = await createDoc(recordCreation);
+      if (recordRes?.err) {
+        console.log(recordRes.msg);
+      }
       return res?._id.toString();
     }
   } catch (error) {
--- services/ingredients/addIngredient.js
+++ services/ingredients/addIngredient.js
@@ -1,30 +1,24 @@
-import { addIngrediant } from '../../controllers/index.js';
-import { getIsraelDateTime } from '../../helpers/getdate.js';
+import { addIngrediant } from "../../controllers/index.js";
+import { getIsraelDateTime } from "../../helpers/getdate.js";
 // Eliran 06/02/24
 // Add Ingredient Service
 
 export const addIngredient = async (req, res) => {
-  if (
-    !req.body?.name ||
-    !req.body?.category ||
-    req.body.info.length() == 0
-  )
-    return res
-      .status(400)
-      .json({ msg: 'params missing' });
+  if (!req.body?.name || !req.body?.category || req.body.info.length() == 0)
+    return res.status(400).json({ msg: "params missing" });
 
   const name = req.body.name;
   const category = req.body.category;
   const info = req.body.info;
   const ingredMeasure = req.body?.ingredMeasure;
-  const ingredScaleToGr =
-    req.body?.ingredScaleToGr;
+  const ingredScaleToGr = req.body?.ingredScaleToGr;
   const allergens = req.body?.allergens;
   const ingredCarbs = req.body?.ingredCarbs;
   const ingredProt = req.body?.ingredProt;
   const ingredSugar = req.body?.ingredSugar;
   const ingredFat = req.body?.ingredFat;
   const ingredSodium = req.body?.ingredSodium;
+  const cUser = req?.userId;
 
   const data = {
     name,
@@ -37,23 +31,15 @@ export const addIngredient = async (req, res) => {
     ingredProt,
     ingredSugar,
     ingredFat,
-    ingredSodium
+    ingredSodium,
+    cUser,
   };
-  
+
   const response = await addIngrediant(data);
 
-
   if (!response?.err) {
-    const docRef="creation";
-    const docType="ingredient";
-    const uDate= new Date(getIsraelDateTime());
-    const userID=req.body?.userID;
-    const docID=response;
-    const data={docRef,docType,uDate,userID,docID};
-    
-
     return res.status(201).json({
-      msg: 'the ingredient was created successfully',
+      msg: "the ingredient was created successfully",
     });
   } else {
     return res.status(400).json(response);
